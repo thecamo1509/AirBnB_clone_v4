58941b635417d690b22c16876f69643994eb26da
Camilo Morales <camiloandres.1509@gmail.com>
2020-05-15 17:18:49 -0500

    Updating script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,14 +6,13 @@ $(document).ready(function () {
 			console.log($(this).attr("data-id"))
 			names.push($(this).attr("data-name"));
 			ids.push($(this).attr("data-id"));
-			$(".amenities h4").text(names)
 		} else {
 			const index = names.indexOf($(this).attr("data-name"));
 			if (index !== -1) {
 				names.splice(index);
 				ids.splice(index);
-				$(".amenities h4").text(names)
 			}
 		}
+		$(".amenities h4").text(names)
 	});
 });
