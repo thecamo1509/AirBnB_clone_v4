d4dba0b0457cc988947835a9ca4f6182ef1cbea6
joldiazch <jluis.diaz@udea.edu.co>
2020-05-15 21:30:36 +0000

    functionality done. styles no good

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,19 @@
-$(document).ready(() => {
-	let names = ["platano"];
+$(document).ready(function () {
+	let names = [];
 	let ids = [];
-	$('input[type="checkbox"]').click(
-		() => {
-			alert(names)
-			if ($(this).prop("checked") === true) {
-				names.push($(this).attr("data-id"));
-				ids.push($(this).attr("data-name"));
-			} else {
-				const index = names.indexOf($(this).attr("data-name"));
-				if (index !== -1) {
-					names.splice(index);
-					ids.splice(index);
-				}
+	$('input[type="checkbox"]').click(function () {
+		if ($(this).prop("checked") === true) {
+			console.log($(this).attr("data-id"))
+			names.push($(this).attr("data-name"));
+			ids.push($(this).attr("data-id"));
+			$(".amenities h4").text(names)
+		} else {
+			const index = names.indexOf($(this).attr("data-name"));
+			if (index !== -1) {
+				names.splice(index);
+				ids.splice(index);
+				$(".amenities h4").text(names)
 			}
 		}
-	);
-	$(".amenities h4").text(names)
+	});
 });
